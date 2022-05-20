fb94f223f1c02fb4a6faac322561571cd49eeefe
Anderson da Silva Machado <anderson361163@gmail.com>
2022-05-20 14:28:13 -0300

    Aplicação base para os testes

---
+++ 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.leilao.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioDaoTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
