9754352197ad8adf6a5e4d5a6e95cc2a6f4002d4
Anderson da Silva Machado <anderson361163@gmail.com>
2022-05-20 21:25:39 -0300

    Atualizacao do projeto parte 2

--- 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,14 +1,47 @@
 package br.com.alura.leilao.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
 
 class UsuarioDaoTest {
 
+	private UsuarioDao dao;
+	
 	@Test
-	void test() {
+	void deveriaEncontrarUsuarioLocazalido() {
+		EntityManager em = JPAUtil.getEntityManager();
+		this.dao = new UsuarioDao(em);
 		
+		Usuario usuario = new Usuario("fulano", "fulano@email.com", "1234567");
+		
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+		
+		Usuario localizado = this.dao.buscarPorUsername(usuario.getNome());
+		Assert.assertNotNull(localizado);
 	}
+	
+	@Test
+	void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+		EntityManager em = JPAUtil.getEntityManager();
+		this.dao = new UsuarioDao(em);
+		
+		Usuario usuario = new Usuario("fulano", "fulano@email.com", "1234567");
+		
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+		
+		Assert.assertThrows(NoResultException.class, ()->this.dao.buscarPorUsername("Anderson"));
+
+	}
+	
 
 }
