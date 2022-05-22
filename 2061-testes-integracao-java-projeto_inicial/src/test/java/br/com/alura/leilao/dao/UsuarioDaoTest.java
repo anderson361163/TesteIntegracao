5d635f22aa2c5eff660ff03abe99dbd84b2a6444
Anderson da Silva Machado <anderson361163@gmail.com>
2022-05-22 15:20:53 -0300

    Refatorando para o modelo Data Test Builder

--- 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
+import br.com.alura.leilao.util.UsuarioBuilder;
 
 class UsuarioDaoTest {
 
@@ -28,35 +29,47 @@ class UsuarioDaoTest {
 		em.getTransaction().rollback();
 	}
 	
+
 	@Test
-	void deveriaEncontrarUsuarioLocazalido() {
-		Usuario usuario = criarUsuario();
+	void deveriaEncontrarUsuarioCadastrado() {
+		Usuario usuario =  new UsuarioBuilder()
+				.comNome("Fulano")
+				.comEmail("fulano@email.com")
+				.comSenha("12345678")
+				.criar();
 		
+		em.persist(usuario);
 		
-		Usuario localizado = this.dao.buscarPorUsername(usuario.getNome());
-		Assert.assertNotNull(localizado);
+		Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
+		Assert.assertNotNull(encontrado);
 	}
-	
+
 	@Test
 	void naoDeveriaEncontrarUsuarioNaoCadastrado() {
-		criarUsuario();
-		EntityManager em = JPAUtil.getEntityManager();
-		this.dao = new UsuarioDao(em);
-		Assert.assertThrows(NoResultException.class, ()->this.dao.buscarPorUsername("Anderson"));
-	}
-	
-	void deveriaRemoverUmUsuario() {
-		Usuario usuario = criarUsuario();
-		dao.deletar(usuario);
-		Assert.assertThrows(NoResultException.class, ()->this.dao.buscarPorUsername(usuario.getNome()));		
-	}
-	
-	private Usuario criarUsuario() {
-		Usuario usuario = new Usuario("fulano", "fulano@email.com", "1234567");
+		Usuario usuario =  new UsuarioBuilder()
+				.comNome("Fulano")
+				.comEmail("fulano@email.com")
+				.comSenha("12345678")
+				.criar();
+		
 		em.persist(usuario);
-		return usuario;
+		
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
 	}
 	
-	
+	@Test
+	void deveriaRemoverUmUsuario() {
+		Usuario usuario =  new UsuarioBuilder()
+				.comNome("Fulano")
+				.comEmail("fulano@email.com")
+				.comSenha("12345678")
+				.criar();
+		
+		em.persist(usuario);
+		
+		dao.deletar(usuario);
+		
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername(usuario.getNome()));
+	}
 
 }
